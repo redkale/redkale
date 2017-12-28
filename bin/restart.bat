e1eece34b72d51cc281a75b3907d8e2f42f07a6b
Redkale <22250530@qq.com>
2017-12-28 11:28:47 +0800

    增加restart脚本

---
+++ bin/restart.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+SET APP_HOME=%~dp0
+
+IF NOT EXIST "%APP_HOME%\conf\application.xml"  SET APP_HOME=%~dp0..
+
+call %APP_HOME%\bin\shutdown.bat 
+
+call %APP_HOME%\bin\start.bat 
---
+++ bin/restart.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export LC_ALL="zh_CN.UTF-8"
+
+APP_HOME=`dirname "$0"`
+
+cd "$APP_HOME"/..
+
+APP_HOME=`pwd`
+
+if [ ! -f "$APP_HOME"/conf/application.xml ]; then 
+     APP_HOME="$APP_HOME"/..  
+fi
+
+cd "$APP_HOME"
+
+./bin/shutdown.sh
+
+./bin/start.sh
+
