93698bacff4f85440cfc6a2443c848a990f840bb
Redkale <22250530@qq.com>
2018-03-07 08:30:15 +0800

    Redkale 1.9.1 开始

--- src/org/redkale/util/Redkale.java
+++ src/org/redkale/util/Redkale.java
@@ -17,7 +17,7 @@ public final class Redkale {
     }
 
     public static String getDotedVersion() {
-        return "1.9.0";
+        return "1.9.1";
     }
 
     public static int getMajorVersion() {
