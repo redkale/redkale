c993cd5b5ab56123eb41b4252286143b49ad1eaa
Redkale <22250530@qq.com>
2018-06-25 19:38:32 +0800

    Json.writeChar以'A'形式输出

--- src/org/redkale/convert/json/JsonWriter.java
+++ src/org/redkale/convert/json/JsonWriter.java
@@ -189,7 +189,7 @@ public class JsonWriter extends Writer {
 
     @Override
     public final void writeChar(char value) {
-        writeInt(value);
+        writeTo('\'', value, '\'');
     }
 
     @Override
