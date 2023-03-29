1dbaf107ca956eac02722c63ec35cc9b38f770a7
redkale <redkale@qq.com>
2023-03-29 21:36:51 +0800

    EntityCache优化

--- src/main/java/org/redkale/source/EntityCache.java
+++ src/main/java/org/redkale/source/EntityCache.java
@@ -807,7 +807,9 @@ public final class EntityCache<T> {
         T old = this.map.putIfAbsent(this.primary.get(rs), rs);
         if (old == null) {
             this.list.add(rs);
-            this.array = transferArray(new ArrayList<>(this.list));
+            if (continuousid) {
+                this.array = transferArray(new ArrayList<>(this.list));
+            }
             return 1;
         } else {
             logger.log(Level.WARNING, this.type + " cache repeat insert data: " + entity);
@@ -824,7 +826,9 @@ public final class EntityCache<T> {
             return 0;
         }
         this.list.remove(rs);
-        this.array[(Integer) primary.get(rs)] = null;
+        if (continuousid) {
+            this.array[(Integer) primary.get(rs)] = null;
+        }
         return 1;
     }
 
@@ -851,7 +855,9 @@ public final class EntityCache<T> {
             ids[++i] = this.primary.get(t);
             this.map.remove(ids[i]);
             this.list.remove(t);
-            this.array[(Integer) primary.get(t)] = null;
+            if (continuousid) {
+                this.array[(Integer) primary.get(t)] = null;
+            }
         }
         return ids;
     }
