1ee5d798666cf124f5ec8054c30cac6a440e5e60
redkale <redkale@qq.com>
2023-11-17 23:05:09 +0800

    删除注释

--- src/main/java/org/redkale/convert/ConvertEnumValue.java
+++ src/main/java/org/redkale/convert/ConvertEnumValue.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.redkale.convert;
 
 import java.lang.annotation.*;
