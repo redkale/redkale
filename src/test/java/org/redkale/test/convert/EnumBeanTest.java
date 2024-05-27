58685dacb250d96151fde96059705386907a08aa
redkale <redkale@qq.com>
2024-05-27 14:26:48 +0800

    format

--- src/test/java/org/redkale/test/convert/EnumBeanTest.java
+++ src/test/java/org/redkale/test/convert/EnumBeanTest.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package org.redkale.test.convert;
 
--- src/test/java/org/redkale/test/convert/proto/PBCustMessage2Test.java
+++ src/test/java/org/redkale/test/convert/proto/PBCustMessage2Test.java
@@ -1,11 +1,13 @@
 /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package org.redkale.test.convert.proto;
 
+import java.lang.annotation.*;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-import java.lang.annotation.*;
 import java.util.Arrays;
 import java.util.function.*;
 import org.junit.jupiter.api.*;
