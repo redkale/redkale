5f5f246c2eb874631b985497796b73ecfaa621e0
redkale <redkale@qq.com>
2024-05-28 19:52:57 +0800

    BiFunctionConvertTest

--- src/test/java/org/redkale/test/convert/BiFunctionConvertTest.java
+++ src/test/java/org/redkale/test/convert/BiFunctionConvertTest.java
@@ -52,7 +52,7 @@ public class BiFunctionConvertTest {
         JsonConvert convert1 = JsonConvert.root();
         System.out.println(convert1.convertTo(table));
         JsonConvert convert2 = convert1.newConvert(
-                (Attribute t, Object u) -> { // 玩家3不输出cards字段
+                (Attribute t, Object u) -> { // 玩家3不输出cards字段 
                     if (t.field().equals("cards") && u instanceof GamePlayer) {
                         int userid = ((GamePlayer) u).userid;
                         if (userid == 3) { // 玩家3的cards不输出
