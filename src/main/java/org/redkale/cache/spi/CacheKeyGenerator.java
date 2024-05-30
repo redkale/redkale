641efad64930e79416e08c4f0cb64f7d1e4a6efe
redkale <redkale@qq.com>
2024-05-30 15:39:33 +0800

    优化CacheKeyGenerator

--- src/main/java/org/redkale/cache/spi/CacheKeyGenerator.java
+++ src/main/java/org/redkale/cache/spi/CacheKeyGenerator.java
@@ -40,7 +40,7 @@ public interface CacheKeyGenerator {
 
     /**
      * 根据MultiHashKey生成一个CacheKeyGenerator
-     * @param key MultiHashKey
+     * @param key MultiHashKey 不能为空
      * @return
      */
     public static CacheKeyGenerator create(MultiHashKey key) {
